b15fc838d660353a8a4e551f9846d78ef5285ff8
Martin Fedec <martin@amplitudesolutions.ca>
2016-11-13 16:29:23 -0800

    Added Travis CI Markdown

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 API for my Bike Manager app
 
+[![Build Status](https://travis-ci.org/amplitudesolutions/bikemanager-api.svg?branch=master)](https://travis-ci.org/amplitudesolutions/bikemanager-api)
+
 Basically this just is a project for me to learn API development and dive into Augular 2.
---
+++ test/test-user.js
@@ -0,0 +1,67 @@
+process.env.NODE_ENV = 'test';
+
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var server = require('../server');
+
+var User = require('../app/models/user');
+
+var should = chai.should();
+
+chai.use(chaiHttp);
+
+describe('Users', function() {
+
+	User.collection.drop();
+
+	beforeEach(function(done) {
+		// When worrying about user, will need to add a new user here... and use through out tests.
+		var newUser = new User({
+			name: 'John Doe',
+			email: 'jdoe@doe.com'
+		});
+
+		newUser.save(function(err, data) {
+			done();
+		})
+	});
+
+	afterEach(function(done) {
+		User.collection.drop();
+		done();
+	});
+
+	it('should get all users /users GET', function(done) {
+		chai.request(server)
+			.get('/api/users')
+			.end(function(err, res) {
+				res.should.have.status(200);
+				res.should.be.json;
+				res.body.should.be.a('array');
+				res.body.length.should.equal(1);
+				res.body[0].should.have.property('_id');
+				res.body[0].should.have.property('name');
+				res.body[0].name.should.equal('John Doe');
+				res.body[0].should.have.property('email');
+				res.body[0].email.should.equal('jdoe@doe.com');
+				done();
+			});
+	});
+
+	it('should add a new user /users POST', function(done) {
+		chai.request(server)
+			.post('/api/users')
+			.send({'name': 'Jane Doe', 'email': 'jane.doe@doe.com'})
+			.end(function(err, res) {
+				res.should.have.status(200);
+				res.should.be.json;
+				res.body.should.be.a('object')
+				res.body.should.have.property('_id');
+				res.body.should.have.property('name');
+				res.body.name.should.equal('Jane Doe');
+				res.body.should.have.property('email');
+				res.body.email.should.equal('jane.doe@doe.com');
+				done();
+			});
+	});
+});
