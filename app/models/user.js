7773a7b49aa8dcf87c882eb6ea99bfef8dadcb15
Martin Fedec <martin@amplitudesolutions.ca>
2016-11-29 20:33:37 -0800

    Fixed issue, back to normal

--- app/models/user.js
+++ app/models/user.js
@@ -4,11 +4,7 @@ var Schema = mongoose.Schema;
 var UserSchema = new Schema({
     name: String,
     email: String,
-    password: {type: String, select: false},
-    employeeId: Integer,
-    about: String,
-    active: { type: Boolean, default: true },
-    deleted: { type: Boolean, default: false }
+    password: {type: String, select: false}
 });
 
 module.exports = mongoose.model("User", UserSchema);
