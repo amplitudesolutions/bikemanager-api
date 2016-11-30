97c61c91769bdd7b08b8bde62365cc22b9c95854
Martin Fedec <martin@amplitudesolutions.ca>
2016-11-29 20:30:12 -0800

    User changes

--- app/models/user.js
+++ app/models/user.js
@@ -4,7 +4,11 @@ var Schema = mongoose.Schema;
 var UserSchema = new Schema({
     name: String,
     email: String,
-    password: {type: String, select: false}
+    password: {type: String, select: false},
+    employeeId: Integer,
+    about: String,
+    active: { type: Boolean, default: true },
+    deleted: { type: Boolean, default: false }
 });
 
 module.exports = mongoose.model("User", UserSchema);
